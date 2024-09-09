4fb18bdbd5874c5c960a7467cfcc3ea2937525b3
Xobaib Mahmoodi <xobaib2001.m@gmail.com>
2024-09-09 14:28:07 +0430

    HomeCards & Card Components

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import "./assets/main.css";
 
+import Card from "@/components/Card.vue";
+
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.component("Card", Card);
+
+app.mount("#app");
