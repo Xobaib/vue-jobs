62d85a0c3be21160a43e1b3b86082dbe36cbfe11
Xobaib Mahmoodi <xobaib2001.m@gmail.com>
2024-09-09 13:58:19 +0430

    Hero Component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
+import Hero from "./components/Hero.vue";
 </script>
 
 <template>
   <Navbar />
-  <h1>Vue Jobs</h1>
+  <Hero />
 </template>
